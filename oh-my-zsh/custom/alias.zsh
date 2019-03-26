eb0b6aa109f2006fae70f4c555a5f1a38be5b13e
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-03-26 21:06:40 +0000

    add zathura configuration file

--- oh-my-zsh/custom/alias.zsh
+++ oh-my-zsh/custom/alias.zsh
@@ -49,3 +49,7 @@ alias dy="youtube-dl --embed-thumbnail -o '~/Videos/Youtube/%(title)s.%(ext)s'"
 alias dys="youtube-dl -x --audio-format mp3 --embed-thumbnail -o '~/Music/Youtube/%(title)s.%(ext)s'"
 
 alias grind="valgrind --leak-check=full --show-reachable=no --show-leak-kinds=all"
+
+function matrix {
+    echo -e "\e[1;40m" ; clear ; while :; do echo $LINES $COLUMNS $(( $RANDOM % $COLUMNS)) $(( $RANDOM % 72 )) ;sleep 0.05; done|awk '{ letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"; c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter; printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }}'
+}
