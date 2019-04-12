bac4b3395c1f9c5b4d48c082b8cc3c6e8bc12e4e
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-04-12 16:51:00 +0100

    VIM is life

--- oh-my-zsh/alias.zsh
+++ oh-my-zsh/alias.zsh
@@ -1,38 +1,39 @@
 alias :r='source ~/.zshrc; '
 alias  c='clear'
 alias stahp='poweroff'
+alias vim="nvim"
 
 #better mpv with plugins
 function mvp {
-    if [ $1 = "" ]; then
+    if [ $# -lt 1 ]; then
         echo "Usage: $0 <file>"
     else
-        mpv --script ~/.config/mpv/scripts/mpris.so $1 &
+        mpv --script ~/.config/mpv/scripts/mpris.so $@ &
         disown
     fi
 }
 
 function pdf {
-    if [ $1 = "" ]; then
+    if [ $# -lt 1 ]; then
         echo "Usage: $0 <file>"
     else
-        zathura $1 &
+        zathura $@ &
         disown
     fi
 }
 
 function png {
-    if [ $1 = "" ]; then
+    if [ $# -lt 1 ]; then
         echo "Usage: $0 <file>"
     else
-        sxiv $1 &
+        sxiv $@ &
         disown
     fi
 }
 
 #speedtest the internet connection
 alias speedtest='speedtest-cli'
-alias speed='speedtest'
+alias speed='speedtest-cli'
 
 #make a directory and cd into it
 mkcd()
@@ -61,3 +62,5 @@ alias grind="valgrind --leak-check=full --show-reachable=no --show-leak-kinds=al
 function matrix {
     echo -e "\e[1;40m" ; clear ; while :; do echo $LINES $COLUMNS $(( $RANDOM % $COLUMNS)) $(( $RANDOM % 72 )) ;sleep 0.05; done|awk '{ letters="abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789@#$%^&*()"; c=$4; letter=substr(letters,c,1);a[$3]=0;for (x in a) {o=a[x];a[x]=a[x]+1; printf "\033[%s;%sH\033[2;32m%s",o,x,letter; printf "\033[%s;%sH\033[1;37m%s\033[0;0H",a[x],x,letter;if (a[x] >= $1) { a[x]=0; } }}'
 }
+
+alias wallup="/home/mightymime/Repos/Nautilus-wallpaper/wallpaper.sh MiEI"
