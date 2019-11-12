96a0e28a65cd65fafc2ade25224168e8fca0b252
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-11-12 11:32:35 +0000

    [zsh] improve git alias

--- zsh/alias/git.zsh
+++ zsh/alias/git.zsh
@@ -1,42 +1,23 @@
-function git_current_branch() {
-  local ref
-  ref=$(command git symbolic-ref --quiet HEAD 2> /dev/null)
-  local ret=$?
-  if [[ $ret != 0 ]]; then
-    [[ $ret == 128 ]] && return  # no git repo.
-    ref=$(command git rev-parse --short HEAD 2> /dev/null) || return
-  fi
-  echo ${ref#refs/heads/}
-}
-
-alias ga='git add'
-
+alias gst='git status'
 alias gb='git branch'
-alias gba='git branch -a'
-alias gbd='git branch -d'
 
-alias gc='git commit -v'
-alias gcam='git commit -a -m'
+alias gco='git checkout'
+alias gcb='git checkout -b'
+alias gcm='git checkout master'
 
+alias gl='git pull'
 gcl() {
     git clone git@github.com:$1
 }
 
-alias gcb='git checkout -b'
-alias gcm='git checkout master'
-alias gcd='git checkout dev'
-alias gco='git checkout'
+alias ga='git add'
 
-alias gd='git diff'
+alias gc='git commit -v'
 
-alias gl='git pull'
 alias gp='git push'
-alias gpsup='git push --set-upstream origin $(git_current_branch)'
+alias gpsup='git push --set-upstream origin $(git branch --show-current)'
 
 alias grhh='git reset --hard'
-alias grm='git rm'
-
-alias gst='git status'
 
 alias glog="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset'"
 alias glogs="git log --graph --pretty='%Cred%h%Creset -%C(auto)%d%Creset %s %Cgreen(%cr) %C(bold blue)<%an>%Creset' --stat"
