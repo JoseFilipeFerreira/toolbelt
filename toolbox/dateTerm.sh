d93f6a48d8d541c0c8323a90502b0f38853fedf0
MightyMime <jose.filipe.matos.ferreira@gmail.com>
2019-11-21 16:27:14 +0000

    [scripts] add lock script

--- toolbox/dateTerm.sh
+++ toolbox/dateTerm.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 p=$(cat $DOTFILES/toolbox/.timetable | grep ^$(date +%u) | awk -v h=$(date +%H) -F: '{ if (h >= $2 && h < $3) {print $4"-UM"}}')
 if [ $p ] && [ -d $MIEI_NOTES/$p ]; then 
     cd $MIEI_NOTES/$p;
