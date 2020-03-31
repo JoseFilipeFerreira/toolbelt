8f70a9f36809e096bd528453ec1c27619e911596
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-03-31 21:03:22 +0100

    [scripts] improve nospace

--- powertools/zsh/alias/dir.zsh
+++ powertools/zsh/alias/dir.zsh
@@ -30,5 +30,7 @@ mkcd() {
 }
 
 nospace() {
-    for file in *; do mv $file $(echo $file | sed -r 's/ - /-/g;s/ _ /_/g;s/,//g;s/\(//g;s/\)//g;s/\!//g;s/ /_/g'); done;
+    for file in *; do
+        mv -vn $file $(echo $file | sed -r "s/['&,()!]//g;s/ - /-/g;s/ _ /_/g;s/ /_/g;s/__/_/g");
+    done;
 }
