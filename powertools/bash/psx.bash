5a02f23558a11f4e4b223b8e9a971d8115dd5a8e
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2020-10-19 12:27:07 +0100

    [bash] fix prompt size

--- powertools/bash/psx.bash
+++ powertools/bash/psx.bash
@@ -1,16 +1,23 @@
 #!/bin/bash
-
 RED="\033[31m"
 GREEN="\033[32m"
 YELLOW="\033[33m"
 BLUE="\033[34m"
 OFF="\033[0m"
 
+__c() {
+    local NO_COLOUR="\e[00m"
+    local PRINTING_OFF="\["
+    local PRINTING_ON="\]"
+    printf "%s%s%s%s%s%s%s" \
+        "$PRINTING_OFF" "$1" "$PRINTING_ON" "$2" "$PRINTING_OFF" "$OFF" "$PRINTING_ON"
+}
+
 PS1_ELEMENTS=()
 
-[ -n "$SSH_CLIENT" ] && PS1_ELEMENTS+=("$YELLOW\u@\h")
+[ -n "$SSH_CLIENT" ] && PS1_ELEMENTS+=("$(__c "$YELLOW" "\u@\h")")
 
-PS1_ELEMENTS+=("$GREEN\w$OFF ")
+PS1_ELEMENTS+=("$(__c "$GREEN" "\w ")")
 
 PS1=$(
     IFS=
