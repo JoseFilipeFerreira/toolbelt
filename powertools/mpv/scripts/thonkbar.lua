639dd66f50645aa5bf39f22f67a67198deb3fa64
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2021-02-21 01:01:36 +0000

    [mpv] add lua script to update thonkbar on events

---
+++ powertools/mpv/scripts/thonkbar.lua
@@ -0,0 +1,12 @@
+function notify_metadata_updated(name, data)
+    notify_current_track()
+end
+
+function update_panel()
+    print("updating")
+    os.execute("pkill --signal 62 thonkbar")
+end
+
+mp.register_event("file-loaded", update_panel)
+mp.register_event("end-file", update_panel)
+mp.observe_property("pause", "bool", update_panel)
