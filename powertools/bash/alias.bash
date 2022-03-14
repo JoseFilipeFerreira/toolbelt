bb4fa82d88e206f4d58bb83c7c3e011ba42692d2
JoseFilipeFerreira <jose.filipe.matos.ferreira@gmail.com>
2022-03-14 15:58:40 +0000

    [install][bash] add exa to worflow

--- powertools/bash/alias.bash
+++ powertools/bash/alias.bash
@@ -3,6 +3,9 @@ alias :q='exit'
 
 alias sudo='sudo '
 
+alias c='clear'
+alias cl='clear; ls -lah'
+
 alias -- -='cd -'
 alias ..='cd ..'
 alias ...='cd ../..'
@@ -10,54 +13,55 @@ alias ....='cd ../../..'
 alias .....='cd ../../../..'
 alias ......='cd ../../../../..'
 
-alias ls='ls --color=auto'
+if hash exa &>/dev/null; then
+    alias ls='exa -g --group-directories-first'
+    alias tree='exa -T'
+else
+    alias ls='ls --color=auto'
+    alias tree='tree -AC'
+fi
 alias l='ls -lah'
 
 alias grep='grep --color=auto --exclude-dir=.git'
 alias watch='watch --color'
-alias tree='tree -AC'
 
-command -V bat &> /dev/null &&
+hash bat &>/dev/null &&
     alias bat='bat --theme=gruvbox-dark -p' &&
     alias cat=bat
 
-command -v neofetch &>/dev/null ||
+hash neofetch &>/dev/null ||
     alias neofetch="curl --silent https://jff.sh/share/neofetch | bash"
-
-alias c='clear'
-alias cl='clear; ls -lah'
 
 alias stahp='poweroff'
 
 alias cleantex='rm *.{aux,idx,log,nav,out,snm,toc,vrb,bbl,blg}(.N) 2>/dev/null'
 
-command -v nvim &>/dev/null &&
-    alias vim="nvim" &&
-    alias viminstall='nvim +:PlugClean +:PlugInstall +:PlugUpdate +:PlugUpgrade'
+hash nvim &>/dev/null &&
+    alias vim="nvim"
 
-command -V python &> /dev/null &&
+hash python &>/dev/null &&
     alias py="python"
 
-command -V stack &>/dev/null &&
+hash stack &>/dev/null &&
     alias ghc="stack ghc" &&
     alias ghci="stack ghci"
 
-command -V valgrind &>/dev/null &&
+hash valgrind &>/dev/null &&
     alias grind="valgrind --leak-check=full --show-reachable=no --show-leak-kinds=all"
 
-command -V tmux &> /dev/null &&
+hash tmux &>/dev/null &&
     alias tmux='tmux -2'
 
-command -V telnet &> /dev/null &&
+hash telnet &>/dev/null &&
     alias starwars='telnet towel.blinkenlights.nl'
 
-command -V oneko &> /dev/null &&
+hash oneko &>/dev/null &&
     alias mi='oneko -tora -name Mi & disown' &&
     alias byemi='killall oneko'
 
-command -V awk &> /dev/null &&
+hash awk &>/dev/null &&
     alias raycaster='awk -f <(curl https://raw.githubusercontent.com/TheMozg/awk-raycaster/master/awkaster.awk)'
 
 # ~/ clean-up
-command -V yarn &> /dev/null &&
+hash yarn &>/dev/null &&
     alias yarn='yarn --use-yarnrc "$XDG_CONFIG_HOME/yarn/config"'
